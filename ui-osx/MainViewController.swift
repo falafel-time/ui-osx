337f58d3635efb6a2075762085500f47e5bcb8b2
Daniel Salmeron Amselem <damselem@google.com>
2015-10-25 20:47:38 +0200

    Create MainViewController

---
+++ ui-osx/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  ui-osx
+//
+//  Created by Daniel Salmeron Amselem on 10/25/15.
+//  Copyright © 2015 falafel-time. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
